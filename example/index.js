5b2e262955c5f03c4a42541694e99a79540505fe
Andrey Popp <8mayday@gmail.com>
2015-01-06 17:15:45 +0800

    Recalculate size on value change
    Fixes #13.

--- example/index.js
+++ example/index.js
@@ -1,8 +1,47 @@
+'use strict';
+
 var React = require('react');
 var TextareaAutosize = require('../');
 
-React.render(
-  React.createElement(
-    TextareaAutosize,
-    {defaultValue: (new Array(15)).join('\nLine.')}),
-  document.getElementById('main'));
+var Demo = React.createClass({
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Controlled mode</h2>
+          <TextareaAutosize
+            value={this.state.value}
+            onChange={this.onChange}
+            />
+          <button onClick={this.changeValueProgramatically}>
+            Change value programatically
+          </button>
+        </div>
+        <div>
+          <h2>Uncontrolled mode</h2>
+          <TextareaAutosize
+            defaultValue={this.state.value}
+            />
+        </div>
+      </div>
+    );
+  },
+
+  getInitialState() {
+    var value = (new Array(15)).join('\nLine.');
+    return {value};
+  },
+
+  onChange(e) {
+    var {value} = e.target;
+    this.setState({value});
+  },
+
+  changeValueProgramatically() {
+    var value = 'This value was set programatically';
+    this.setState({value});
+  }
+});
+
+React.render(<Demo />, document.getElementById('main'));
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var React = require('react');
 var objectAssign = require('object-assign');
 
@@ -14,57 +16,55 @@ var TextareaAutosize = React.createClass({
   },
 
   componentDidMount: function() {
-    this.getDiffSize();
     this.recalculateSize();
   },
 
-  componentWillReceiveProps: function(nextProps) {
-    this.dirty = !!nextProps.style;
-  },
-
-  componentDidUpdate: function() {
-    if (this.dirty) {
-      this.getDiffSize();
-      this.dirty = false;
+  componentDidUpdate: function(prevProps) {
+    if (
+      prevProps.style
+      || prevProps.value !== this.props.value
+      || this.props.value == undefined
+    ) {
+      this.recalculateSize();
     }
   },
 
-  onChange: function(e) {
+  onChange(e) {
     if (this.props.onChange) {
       this.props.onChange(e);
     }
-    this.recalculateSize();
+    if (this.props.value == undefined) {
+      // controlled mode
+      this.recalculateSize();
+    }
   },
 
-  getDiffSize: function() {
+  recalculateSize: function() {
+    var diff;
+    var node = this.getDOMNode();
+
     if (window.getComputedStyle) {
-      var styles = window.getComputedStyle(this.getDOMNode());
+      var styles = window.getComputedStyle(node);
 
       // If the textarea is set to border-box, it's not necessary to
       // subtract the padding.
       if (styles.getPropertyValue('box-sizing') === "border-box" ||
           styles.getPropertyValue('-moz-box-sizing') === "border-box" ||
           styles.getPropertyValue('-webkit-box-sizing') === "border-box") {
-        this.diff = 0;
+        diff = 0;
       } else {
-        this.diff = (
-            parseInt(styles.getPropertyValue('padding-bottom') || 0, 10) +
-            parseInt(styles.getPropertyValue('padding-top') || 0, 10)
-            );
+        diff = (
+          parseInt(styles.getPropertyValue('padding-bottom') || 0, 10) +
+          parseInt(styles.getPropertyValue('padding-top') || 0, 10)
+        );
       }
     } else {
-      this.diff = 0;
-    }
-  },
-
-  recalculateSize: function() {
-    if (!this.isMounted()) {
-      return;
+      diff = 0;
     }
 
     var node = this.getDOMNode();
     node.style.height = 'auto';
-    node.style.height = (node.scrollHeight - this.diff) + 'px';
+    node.style.height = (node.scrollHeight - diff) + 'px';
   }
 });
 
