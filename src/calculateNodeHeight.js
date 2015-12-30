6264b7a3008ffdb7173d1a5087c5adf0e69304d5
Hunmin, Kim <jeokrang@gmail.com>
2015-12-30 18:04:50 +0900

    Fix resize error in ie11 when textarea is empty
    https://github.com/andreypopp/react-textarea-autosize/issues/74

--- src/calculateNodeHeight.js
+++ src/calculateNodeHeight.js
@@ -69,7 +69,7 @@ export default function calculateNodeHeight(uiTextNode,
 
   if (minRows !== null || maxRows !== null) {
     // measure height of a textarea with a single row
-    hiddenTextarea.value = '';
+    hiddenTextarea.value = '\uFEFF';
     let singleRowHeight = hiddenTextarea.scrollHeight - paddingSize;
     if (minRows !== null) {
       minHeight = singleRowHeight * minRows;
