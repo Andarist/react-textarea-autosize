136a1a6bdbf855b69ddd3304bdb78fad77db278f
Mateusz Burzyński <mateuszburzynski@gmail.com>
2017-05-03 12:19:19 +0200

    Tweaked rollup setup + remade all tasks to npm scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,24 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
 
-export default {
-  entry: 'src/TextareaAutosize.js',
-  dest: 'dist/TextareaAutosize.min.js',
+const env = process.env.NODE_ENV;
+
+const config = {
   format: 'umd',
   moduleName: 'TextareaAutosize',
   plugins: [
+    nodeResolve({
+      jsnext: true
+    }),
     babel({
       exclude: 'node_modules/**',
       plugins: ['external-helpers'],
     }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })
   ],
   external: [
     'react', 'prop-types'
@@ -19,3 +28,25 @@ export default {
     'prop-types': 'PropTypes'
   },
 };
+
+if (env === 'production') {
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false,
+        screw_ie8: true
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      output: {
+        screw_ie8: true
+      }
+    })
+  );
+}
+
+export default config;
