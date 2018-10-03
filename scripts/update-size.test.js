e27f5aca9cca1b3f2705976b36fc14ac17227cf7
selvaganesh <selvaganeshrajam@gmail.com>
2018-10-03 23:13:14 +0530

    remove unnecessary require modules

--- scripts/update-size.test.js
+++ scripts/update-size.test.js
@@ -1,16 +1,11 @@
-const fs = require('fs');
 const path = require('path');
-const sizeLimit = require('size-limit');
 const root = path.join(__dirname, '..');
-const readmePath = path.join(root, 'README.md');
-const readme = fs.readFileSync(readmePath, 'utf-8');
 const pkg = require(path.join(root, 'package.json'));
 const file = path.join(root, pkg.browser[pkg.module]);
 const size = require('./update-size');
 
 describe('size-limit', () => {
   it('check size to be object', () => {
-    let resultData = {};
     return size
       .sizeLimit(file, { ignore: ['react', 'prop-types'] })
       .then(result => {
@@ -19,7 +14,6 @@ describe('size-limit', () => {
   });
 
   it('check gzip size limit', () => {
-    let resultData = {};
     return size
       .sizeLimit(file, { ignore: ['react', 'prop-types'] })
       .then(result => {
@@ -28,7 +22,6 @@ describe('size-limit', () => {
   });
 
   it('check parsed size limit', () => {
-    let resultData = {};
     return size
       .sizeLimit(file, { ignore: ['react', 'prop-types'] })
       .then(result => {
