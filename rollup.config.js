6176a51d7c599f37b8835e44d182f32f194a1b56
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-10-29 23:25:59 +0100

    Use scoped @rollup/plugin-replace

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
+import replace from '@rollup/plugin-replace';
 import { uglify } from 'rollup-plugin-uglify';
 import pkg from './package.json';
 
