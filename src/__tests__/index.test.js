2cd507787d7eeebb55b8c26d8f5ec39b306c1920
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-10-29 23:13:51 +0100

    Use scoped version of react-testing-library

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render } from 'react-testing-library';
+import { render } from '@testing-library/react';
 import TextareaAutosize from '../index';
 import 'jest-dom/extend-expect';
 
