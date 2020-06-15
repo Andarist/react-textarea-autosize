0edfc2764a753df28ac29ddbdbc1cff96db8e6e7
Matteo Martinelli <matteomartinelli1992@gmail.com>
2020-06-15 22:51:41 +0200

    fix: add `!important` to inline height property

--- src/index.tsx
+++ src/index.tsx
@@ -75,7 +75,7 @@ const TextareaAutosize: React.ForwardRefRenderFunction<
 
     if (heightRef.current !== height) {
       heightRef.current = height;
-      node.style.height = `${height}px`;
+      node.style.setProperty(height, `${height}px`, 'important');
       onHeightChange(height);
     }
   };
