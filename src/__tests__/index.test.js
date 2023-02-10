30206722eccf3cd9f073514a069d40baf5d3c618
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2023-02-10 09:39:35 +0100

    Add JSDom and mock document.fonts listner methods

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,9 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
 import '@testing-library/jest-dom/extend-expect';
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import TextareaAutosize from '../index';
 
 describe('<TextareaAutosize />', () => {
+  beforeAll(() => {
+    // Unfortunately, JSDom does not implement document.fonts yet
+    // so we're mocking it
+    Object.defineProperty(document, 'fonts', {
+      value: { addEventListener() {}, removeEventListener() {} },
+    });
+  });
   it('renders ok', () => {
     const { asFragment } = render(<TextareaAutosize />);
 
